16dd4fc742ce4f0b9b6847fa5e498cd89e4e1191
Peter Watters <watterspeterswin@msn.com>
2015-08-03 21:39:47 -0700

    new create & delete page functions

--- public/manage_content.php
+++ public/manage_content.php
@@ -25,6 +25,7 @@
 				echo "<br/>   Visible: ";
 				echo $current_subject["visible"]==0 ? "no" : "yes";
 				echo "<br/><br/><a href=\"edit_subject.php?subject={$current_subject["id"]}\">Edit Subject</a>";
+				echo "<br/><br/><a href=\"new_page.php?subject={$current_subject["id"]}\">Add Page</a>";
 			}
 			elseif ($current_page) {
 				echo "<h2>Manage Page</h2>";
@@ -37,6 +38,9 @@
 				echo "<div class=\"view-content\">";
 				echo htmlentities($current_page["content"]);
 				echo "</div>";
+				echo "<br/><br/><a href=\"new_page.php?subject={$current_page["subject_id"]}\">Add Page</a>";
+				echo "<br/><br/><a href=\"delete_page.php?page={$current_page["id"]}\">Delete Page</a>";
+
 			}
 			else {
 				echo "Please select a subject or page";
